417055281df587860a61109b106ceb5fb8475014
Kyle Nissen <nissenkyle@gmail.com>
2014-02-05 12:03:13 -0700

    Added mappings to all the xbox controller buttons and axes.  Moved the input polling loop into the input class and also added a mailbox to the class.

--- component/input/input.cpp
+++ component/input/input.cpp
@@ -1,17 +1,39 @@
 #include <stdint.h>
 #include "input.h"
 #include "../../Standard.h"
+#include "../events/events.h"
 
 Input::Input() {	
 }
 
 Input::~Input() {
-	SDL_JoystickClose(joystick1);
+	SDL_JoystickClose(m_joystick1);
+
+	m_pMailbox = new Events::Mailbox();
 }
 
 void Input::OpenJoysticks(){
 	SDL_JoystickEventState(SDL_ENABLE);
-	joystick1 = SDL_JoystickOpen(0);
+	m_joystick1 = SDL_JoystickOpen(0);
+	int number_of_buttons;
+	number_of_buttons = SDL_JoystickNumButtons(m_joystick1);
+	DEBUGOUT("Joystick %i opened with %i buttons", m_joystick1, number_of_buttons);
+}
+
+void Input::HandleEvents(){
+
+	SDL_Event event;
+	Events::Event *outgoingInputEvents = new Events::Event(Events::EventType::KartMove);
+
+	//10ms into the future, ensures the input loop doesn't last longer than 10ms
+	Uint32 timeout = SDL_GetTicks() + 20;   
+
+	while( SDL_PollEvent(&event) && !SDL_TICKS_PASSED(SDL_GetTicks(), timeout) )
+	{
+		OnEvent(&event);
+
+		//Add code to handle compiling events into one outgoing mail item
+	}
 }
 
 void Input::OnEvent(SDL_Event* Event) {
@@ -40,11 +62,12 @@ void Input::OnEvent(SDL_Event* Event) {
 	case SDL_KEYUP:
 		OnKeyUp(Event->key.keysym.scancode, Event->key.keysym.mod, Event->key.type);
 		break;
-	//case SDL_JOYAXISMOTION:
-	//	OnControllerAxisMotion(Event->jaxis.axis);
-	//case SDL_CONTROLLERAXISMOTION:
-	//	OnControllerAxisMotion(Event->caxis);
-	//	break;
+	case SDL_JOYAXISMOTION:
+		OnJoystickAxisMotion(Event->jaxis);
+		break;
+	case SDL_JOYBUTTONDOWN:
+		OnJoystickButton(Event->jbutton);
+		break;
 	default:
 		//DEBUGOUT("SDL Event type:%i", Event->type);
 		break;
@@ -72,7 +95,55 @@ void Input::OnKeyUp(SDL_Scancode scancode, Uint16 mod, Uint32 type){
 	}
 }
 
-void Input::OnControllerAxisMotion(SDL_ControllerAxisEvent event){
-	int x = 0;
-	x++;
+void Input::OnJoystickAxisMotion(SDL_JoyAxisEvent event){
+
+	// Used to scale joystick inputs to range of -1 to 1
+	Sint16 scaleFactor = 32767;
+
+	if( (event.value >= MIN_JOY_MOVEMENT_THRESHOLD ) || (event.value <= -MIN_JOY_MOVEMENT_THRESHOLD) )
+	{
+		switch (event.axis)
+		{
+		case LEFT_STICK_LEFT_RIGHT_AXIS:
+			break;
+		case LEFT_STICK_UP_DOWN_AXIS:
+			break;
+		case RIGHT_STICK_LEFT_RIGHT_AXIS:
+			break;
+		case RIGHT_STICK_UP_DOWN_AXIS:
+			break;
+		case LEFT_TRIGGER_AXIS:
+			break;
+		case RIGHT_TRIGGER_AXIS:
+			break;
+		default:
+			break;
+		}
+	}
+
+}
+
+void Input::OnJoystickButton(SDL_JoyButtonEvent event){
+	//DEBUGOUT("Joystick button: %i\n", event.button);
+	switch (event.button)
+	{
+	case A_BUTTON:
+		break;
+	case X_BUTTON:
+		break;
+	case B_BUTTON:
+		break;
+	case Y_BUTTON:
+		break;
+	case LEFT_SHOULDER_BUTTON:
+		break;
+	case RIGHT_SHOULDER_BUTTON:
+		break;
+	case START_BUTTON:
+		break;
+	case BACK_BUTTON:
+		break;
+	default:
+		break;
+	}
 }
--- component/input/input.h
+++ component/input/input.h
@@ -1,11 +1,42 @@
 #pragma once
 
 #include <SDL.h>
+#include "../events/events.h"
 
 class Input {
+
+private:
+
+	SDL_Joystick *m_joystick1;
+	Events::Mailbox *m_pMailbox;
+
 public:
 
-	SDL_Joystick *joystick1;
+	// X-BOX controller button mappings
+	int static const D_PAD_UP_BUTTON = 0;
+	int static const D_PAD_DOWN_BUTTON = 1;
+	int static const D_PAD_LEFT_BUTTON = 2;
+	int static const D_PAD_RIGHT_BUTTON = 3;
+	int static const START_BUTTON = 4;
+	int static const BACK_BUTTON = 5;
+	int static const LEFT_THUMB_STICK_BUTTON = 6;	
+	int static const RIGHT_THUMB_STICK_BUTTON = 7;
+	int static const LEFT_SHOULDER_BUTTON = 8;
+	int static const RIGHT_SHOULDER_BUTTON = 9;
+	int static const A_BUTTON = 10;
+	int static const B_BUTTON = 11;
+	int static const X_BUTTON = 12;
+	int static const Y_BUTTON = 13;
+
+	// X-BOX controller joystick axis mappings
+	int static const LEFT_STICK_LEFT_RIGHT_AXIS = 0;
+	int static const LEFT_STICK_UP_DOWN_AXIS = 1;
+	int static const RIGHT_STICK_LEFT_RIGHT_AXIS = 2;
+	int static const RIGHT_STICK_UP_DOWN_AXIS = 3;
+	int static const LEFT_TRIGGER_AXIS = 4;
+	int static const RIGHT_TRIGGER_AXIS = 5;
+
+	int static const MIN_JOY_MOVEMENT_THRESHOLD = 3200;
 
 	Input();
 
@@ -13,12 +44,15 @@ public:
 
 	void OpenJoysticks();
 
+	void HandleEvents();
 	void OnEvent(SDL_Event* Event);
 
+	// Keyboard events
 	void OnKeyDown(SDL_Scancode scancode, Uint16 mod, Uint32 type);
-
 	void OnKeyUp(SDL_Scancode scancode, Uint16 mod, Uint32 type);
 
-	void OnControllerAxisMotion(SDL_ControllerAxisEvent event);
+	//Joystick Events
+	void OnJoystickAxisMotion(SDL_JoyAxisEvent event);
+	void OnJoystickButton(SDL_JoyButtonEvent event);
 
 };
