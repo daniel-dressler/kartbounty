2dbd58d1403a97211afc26a5a5513cc99170dcfe
Kyle Nissen <nissenkyle@gmail.com>
2014-03-24 22:54:55 -0600

    Killing karts now gives you points Press 'enter' to use power-ups Turned the STEERING_MAX_ANGLE down, way too sensitive on keyboard input

--- component/input/input.cpp
+++ component/input/input.cpp
@@ -216,6 +216,9 @@ void Input::OnKeyDown(SDL_Keycode keycode, Uint16 mod, Uint32 type){
 	case SDLK_LSHIFT:
 		m_pCurrentInput->aPressed = true;
 		break;
+	case SDLK_RETURN:
+		m_pCurrentInput->xPressed = true;
+		break;
 
 		// Development Tools
 	case SDLK_z:
@@ -255,6 +258,9 @@ void Input::OnKeyUp(SDL_Keycode keycode, Uint16 mod, Uint32 type){
 		break;
 	case SDLK_LSHIFT:
 		m_pCurrentInput->aPressed = false;
+		break;
+	case SDLK_RETURN:
+		m_pCurrentInput->xPressed = false;
 		break;
 	default:
 		break;
