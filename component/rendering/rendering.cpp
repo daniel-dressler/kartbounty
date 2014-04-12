3b575a4a5094a23d9a2c3678602578376deee271
Philip Whittingham <philip.whittingham@gmail.com>
2014-04-12 00:09:16 -0600

    Fixed gl version

--- component/rendering/rendering.cpp
+++ component/rendering/rendering.cpp
@@ -120,8 +120,8 @@ int LoadMesh( GLmesh& mesh, char* strFilename )
 int Renderer::setup()
 {
 	SDL_Init( SDL_INIT_EVERYTHING );
-	SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 4 );
-	SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 4 );
+	SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
+	SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 3 );
 	SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 16 );
 	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
 	SDL_GL_SetAttribute( SDL_GL_MULTISAMPLEBUFFERS, 1 );
